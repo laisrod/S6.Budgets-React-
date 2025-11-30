314c56bd0e52495e10b8f1fd3f386cd1147b35b9
Laís Rodrigues <devlaisrodrigues@gmail.com>
2025-11-30 14:11:40 -0300

    feat: add HelpButton component with info icon and export

---
+++ orcamentos-react/src/components/atoms/HelpButton.tsx
@@ -0,0 +1,20 @@
+interface HelpButtonProps {
+    onClick: () => void
+    ariaLabel?: string
+}
+
+export default function HelpButton({ onClick, ariaLabel = 'Abrir ajuda' }: HelpButtonProps) {
+    return (
+        <button
+        type="button"
+        onClick={onClick}
+        className="help-button"
+        aria-Label={ariaLabel}
+        title="Clique para mais informações"
+        >
+
+            <span className="help-icon">ℹ️</span>
+        </button>
+
+    )
+}
